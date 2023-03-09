8543e3a2ad4d71eff2e63ec66b41e7230d0b79b5
Sergey Trofimov <sergey@trofimov.name>
2023-03-09 14:37:39 +0100

    (chore): Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/zmap "1.3.26"
+(defproject com.github.strojure/zmap "1.3.27-SNAPSHOT"
   :description "Persistent maps with support for lazy values."
   :url "https://github.com/strojure/zmap"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
