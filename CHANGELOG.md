a731069b7ce05d5421f07402a64b872b6937ca01
Sergey Trofimov <sergey@trofimov.name>
2023-03-09 14:27:55 +0100

    (build): Add `cljs-test` lein alias

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 Release date `UNRELEASED`
 
 - (chore): Change license to Unlicense.
+- (build): Add `cljs-test` lein alias.
 
 ## `1.3.18`
 
--- project.clj
+++ project.clj
@@ -7,10 +7,15 @@
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]
                                        [org.clojure/clojurescript "1.11.60"]]}
-             :dev,,,,, {:dependencies [;; clojurescript repl deps
+             :dev,,,,, {:dependencies [;; clojurescript tests
                                        [com.google.guava/guava "31.1-jre"]
+                                       [olical/cljs-test-runner "3.8.0"]
                                        ;; competitors
                                        [malabarba/lazy-map "1.3"]]
                         :source-paths ["doc"]}}
+
+  :aliases {"cljs-test" ["run" "-m" "cljs-test-runner.main"]}
+
+  :clean-targets ["target" "cljs-test-runner-out"]
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]])
